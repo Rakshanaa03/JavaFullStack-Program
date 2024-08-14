a495d8637ce915282ce0ec7cedddfd233a976e9a
Rakshanaa03 <blackway141@gmail.com>
2024-08-14 09:07:20 +0530

    today

---
+++ fullstack/Java Eclipse/Employee.java
@@ -0,0 +1,59 @@
+package com.rakshanaa;
+class Employee {
+ private String name;
+ private String jobTitle;
+ private double salary;
+
+ public Employee(String name, String jobTitle, double salary) {
+     this.name = name;
+     this.jobTitle = jobTitle;
+     this.salary = salary;
+ }
+
+ public String getName() {
+     return name;
+ }
+
+ public void setName(String name) {
+     this.name = name;
+ }
+
+ public String getJobTitle() {
+     return jobTitle;
+ }
+
+ public void setJobTitle(String jobTitle) {
+     this.jobTitle = jobTitle;
+ }
+
+ public double getSalary() {
+     return salary;
+ }
+
+ public void setSalary(double salary) {
+     this.salary = salary;
+ }
+
+ public void applyRaise(double percentage) {
+     if (percentage > 0) {
+         salary += salary * (percentage / 100);
+     }
+ }
+
+ public void printDetails() {
+     System.out.println("Name: " + name);
+     System.out.println("Job Title: " + jobTitle);
+     System.out.println("Salary: $" + salary);
+ }
+
+ public static void main(String[] args) {
+     Employee employee = new Employee("Rakshanaa", "Software Engineer", 85000);
+     System.out.println("Initial Employee Details:");
+     employee.printDetails();
+     System.out.println(); 
+     employee.applyRaise(10);
+     System.out.println("Updated Employee Details:");
+     employee.printDetails();
+ }
+}
+
---
+++ fullstack/Java Eclipse/Person.java
@@ -0,0 +1,32 @@
+package com.rakshanaa;
+
+public class Person {
+
+	 private String name;
+	    private int age;
+
+	   
+	    public Person(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	   
+	    public void printDetails() {
+	        System.out.println("Name: " + name);
+	        System.out.println("Age: " + age);
+	    }
+	
+	    public static void main(String[] args) {
+	       
+	        Person person1 = new Person("rakshanaa", 30);
+	        Person person2 = new Person("Dhiya", 25);
+ 
+	        System.out.println("Details of Person 1:");
+	        person1.printDetails();
+	        System.out.println();
+
+	        System.out.println("Details of Person 2:");
+	        person2.printDetails();
+	    }
+	}
